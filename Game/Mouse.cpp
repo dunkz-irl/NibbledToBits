47786f948c30cd46eee029e3e3e505b6700941b2
dunkz-irl <djgallagher93@gmail.com>
2023-03-07 10:55:59 +0000

    Add subtle variation to mouse animation speeds and start frames for variety

--- Game/Mouse.cpp
+++ Game/Mouse.cpp
@@ -6,11 +6,15 @@ Mouse::Mouse(Play::Point2f pos)
 	m_matrix.row[2].x = pos.x;
 	m_matrix.row[2].y = pos.y;
 
-	m_animSpeed = 19.f;
+	m_animSpeed = 19.f + Play::RandomRollRange(-100, 100) / 100.f;
+	m_animFrame = Play::RandomRoll(8) - 1; // #TODO: Assumes there will always be 8 frames which might not be true of other animation states
+
 	m_movementSpeed = 2.f;
 
 	// Set sprite to one of three
 	m_spriteID = Play::GetSpriteId(m_spriteNames[Play::RandomRoll(3) - 1]);
+
+	
 }
 
 void Mouse::Update()
